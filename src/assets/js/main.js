14fb57205b3ae1c04bfe0148157b646472b717f5
Oddish <zumiksx@gmail.com>
2023-08-06 14:18:54 +0300

    Save

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,11 @@
+// Bootstrap
+import * as bootstrap from 'bootstrap'
+import * as Popper from "@popperjs/core"
+
+// Slider
 new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
 });
-
